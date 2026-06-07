71a8e37359057bc3ea50d714219e92554b2eba44
Yash Gawad <krutikagawad5@gmail.com>
2026-06-07 15:53:56 +0530

    Solved className problem

--- projects/7-social-media/src/components/Post.jsx
+++ projects/7-social-media/src/components/Post.jsx
@@ -1,9 +1,9 @@
 const Post = () => {
   return (
-    <div class="card" style={{width: "18rem"}}>
-      <img src="..." class="card-img-top" alt="..." />
-      <div class="card-body">
-        <p class="card-text">
+    <div className="card" style={{ width: "18rem" }}>
+      <img src="..." className="card-img-top" alt="..." />
+      <div className="card-body">
+        <p className="card-text">
           Some quick example text to build on the card title and make up the
           bulk of the card’s content.
         </p>
