d39478f55307ea35b54b16902b645b8146ea6442
Yash Gawad <krutikagawad5@gmail.com>
2026-06-29 18:37:42 +0530

    Impeoved bharat clock project

---
+++ projects/8-bharat-clock-version-two/src/components/CurrentTime.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+let CurrentTime = ()=> {
+  const [time, setTime] = useState(new Date);
+
+  useEffect(() => {
+    const intervalid = setInterval(() => {
+      setTime(new Date)
+    }, 1000)
+
+  return () => {
+    clearInterval(intervalid);
+  }
+  }, [])
+
+  return <p className="lead">This is the current time: {time.toLocaleDateString()} - {time.toLocaleTimeString()}</p>
+}
+
+export default CurrentTime
